a1b672d92c8a7df4115ac5a759bb9af74c838043
AhmadReza Harati <104574670+TheLemonHead@users.noreply.github.com>
2022-05-09 22:04:00 +0430

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Lemonica',
+      title: 'Lemonica Game Studio',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
